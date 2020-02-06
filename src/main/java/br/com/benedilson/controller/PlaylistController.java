0ea0c71d775f56622a9cf679df4502f09f000f5e
Benedilson de Jesus  Batista de Lima <benedilson.lima@infosolo.com.br>
2020-02-06 16:04:36 -0200

    adicionando tela add.html

---
+++ src/main/java/br/com/benedilson/controller/PlaylistController.java
@@ -0,0 +1,46 @@
+package br.com.benedilson.controller;
+
+import br.com.benedilson.domain.Playlist;
+import br.com.benedilson.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("playlists")
+public class PlaylistController {
+
+    @Autowired
+    private PlaylistService playlistService;
+
+    @GetMapping("/listar")
+    public ModelAndView listar(ModelMap model) {
+        model.addAttribute("playlists", playlistService.recuperar());
+        return new ModelAndView("playlist/list", model);
+    }
+
+    @GetMapping("/cadastro")
+    public String preSalvar(@ModelAttribute("playlist") Playlist playlist) {
+        return "/playlist/add";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@Valid @ModelAttribute("playlist") Playlist playlist, BindingResult result, RedirectAttributes attr) {
+        if(result.hasErrors()) {
+            return "playlist/add";
+        }
+
+        playlistService.salvar(playlist);
+        attr.addFlashAttribute("mensagem", "Playlist criada com sucesso.");
+        return "redirect:/playlists/listar";
+    }
+}
